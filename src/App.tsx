9fa62c2495d6d0efbd458ea24313bbe6c4938149
Demetrius Panovitch <demetrius.mp789@gmail.com>
2024-10-16 18:38:50 -0400

    feat: initial energy supplier card layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Container } from "@/components/container";
+import { EnergySupplierCard } from "@/components/energy-supplier-card";
 import { MonthlyConsumptionForm } from "@/components/forms/monthly-consumption-form";
 import { PageHeading } from "@/components/page-heading";
 import { Separator } from "@/components/ui/separator";
@@ -11,9 +12,27 @@ function App() {
         description="Iremos buscar os melhores fornecedores de acordo com as suas necessidades."
       />
 
-      <Separator className="mb-8 mt-6" />
-
       <MonthlyConsumptionForm />
+
+      <Separator className="my-8" />
+
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8">
+        {Array.from({ length: 5 }).map((_, i) => {
+          return (
+            <EnergySupplierCard
+              key={i}
+              id="demetrius.mp789@gmail.com"
+              logo="https://picsum.photos/200"
+              name="Energisa"
+              state="Mato Grosso do Sul"
+              costPerKwh={0.9}
+              minKwhLimit={15000}
+              totalClients={1000}
+              averageRating={i + Math.random()}
+            />
+          );
+        })}
+      </div>
     </Container>
   );
 }
---
+++ src/components/energy-supplier-card.tsx
@@ -0,0 +1,115 @@
+import { Typography } from "@/components/typography";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Star, StarHalf } from "lucide-react";
+
+type EnergySupplierCardProps = {
+  id: string;
+  name: string;
+  logo: string;
+  state: string;
+  costPerKwh: number;
+  minKwhLimit: number;
+  totalClients: number;
+  averageRating: number;
+};
+
+function getInitials(name: string) {
+  const splitted = name.split(" ");
+
+  if (splitted.length === 2) {
+    const firstChar = splitted[0][0];
+    const secondChar = splitted[1][0];
+
+    return `${firstChar}${secondChar}`.toUpperCase();
+  }
+
+  return name.slice(0, 2).toUpperCase();
+}
+
+const currencyFormatter = new Intl.NumberFormat("pt-BR", {
+  style: "currency",
+  currency: "BRL",
+});
+
+export function EnergySupplierCard(props: EnergySupplierCardProps) {
+  const {
+    name,
+    logo,
+    state,
+    costPerKwh,
+    minKwhLimit,
+    totalClients,
+    averageRating,
+  } = props;
+
+  const numberOfFullStars = Math.floor(averageRating + Number.EPSILON);
+  const hasHalfStar = averageRating - numberOfFullStars >= 0.5;
+  const numberOfEmptyStars = 5 - numberOfFullStars - (hasHalfStar ? 1 : 0);
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="flex items-center gap-2">
+          <Avatar className="h-8 w-8">
+            <AvatarImage src={logo} alt="@shadcn" />
+            <AvatarFallback className="text-sm">
+              {getInitials(name)}
+            </AvatarFallback>
+          </Avatar>
+
+          {name}
+        </CardTitle>
+        <CardDescription>{state}</CardDescription>
+      </CardHeader>
+      <CardContent className="-mt-4">
+        <div className="flex items-center gap-1">
+          <div className="flex">
+            {Array.from({ length: numberOfFullStars }).map((_, i) => {
+              return <Star fill="black" strokeWidth={0} key={i} />;
+            })}
+
+            {hasHalfStar && <StarHalf fill="black" />}
+
+            {Array.from({ length: numberOfEmptyStars }).map((_, i) => {
+              return <Star key={i} />;
+            })}
+          </div>
+
+          <Typography variant="muted">
+            ({Math.round((averageRating + Number.EPSILON) * 100) / 100} de 5)
+          </Typography>
+        </div>
+
+        <ul className="ml-6 list-disc [&>li]:mt-2">
+          <li>
+            Custo por kWh:{" "}
+            <span className="font-semibold">
+              {currencyFormatter.format(costPerKwh)}
+            </span>
+          </li>
+          <li>
+            Limite mínimo de kWh:{" "}
+            <span className="font-semibold">{minKwhLimit} kWh</span>
+          </li>
+          <li>
+            Quantidade de clientes:{" "}
+            <span className="font-semibold">{totalClients}</span>
+          </li>
+        </ul>
+      </CardContent>
+
+      <CardFooter className="flex">
+        <Button className="w-full">Receber proposta</Button>
+      </CardFooter>
+    </Card>
+  );
+}
