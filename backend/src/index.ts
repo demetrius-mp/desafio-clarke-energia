a043a381c081304a7e41be9712a33318b6db47fe
Demetrius Panovitch <demetrius.mp789@gmail.com>
2024-10-16 22:57:21 -0400

    feat: prisma and express
    added endpoint to fetch energy suppliers using prisma

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,33 @@
-import { CONSTANT } from "@/lib";
+import { prisma } from "@/lib/prisma";
+import express from "express";
+import { z } from "zod";
 
-console.log(CONSTANT);
+const app = express();
+
+const EnergySupplierQueryParamsSchema = z.object({
+  monthlyConsumption: z.coerce.number().positive(),
+});
+
+app.get("/energy-suppliers", async (req, res) => {
+  const parsed = EnergySupplierQueryParamsSchema.safeParse(req.query);
+
+  if (!parsed.success) {
+    res.status(400).json(parsed.error);
+    return;
+  }
+
+  const { monthlyConsumption } = parsed.data;
+
+  const energySuppliers = await prisma.energySupplier.findMany({
+    where: {
+      minKwhLimit: {
+        lte: monthlyConsumption,
+      },
+    },
+  });
+
+  res.json(energySuppliers);
+  return;
+});
+
+app.listen(3000);
