9399ebfb86695502ea7c3f3a648005db68d384bb
Demetrius Panovitch <demetrius.mp789@gmail.com>
2024-10-17 13:58:28 -0400

    feat: start nextjs app

---
+++ nextjs/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Buscar fornecedores",
+};
+
+export default function Home() {
+  return <h1 className="text-2xl">Hello world!</h1>;
+}
